944268240680560e4b60e404d036a25e03260f0a
Surya Prasad <techniboy.h2o@gmail.com>
2021-07-16 16:51:46 +0100

    initial commit

---
+++ src/components/player/ControlBar.js
@@ -0,0 +1,25 @@
+import { React } from 'react';
+import { Row } from 'react-bootstrap/Row';
+import { Container } from 'react-bootstrap/Container';
+import { Col } from 'react-bootstrap/Col';
+import { Button } from 'react-bootstrap/Button';
+
+function ControlBar() {
+    return (
+        <Container fluid>
+            <Row>
+                <Col>
+                    <Button>
+                        prev
+                    </Button>
+                </Col>
+                <Col>
+                    
+                </Col>
+                <Col>
+                    
+                </Col>
+            </Row>
+        </Container>
+    )
+}
